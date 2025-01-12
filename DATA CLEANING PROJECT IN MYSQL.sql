b09fec82d9b3bbcdf40986276679a42644341149
Chisom-ruth <chisomruth556@gmail.com>
2025-01-13 01:28:28 +1300

    Update DATA CLEANING PROJECT IN MYSQL.sql
    This project focuses on cleaning a layoff dataset using MySQL to ensure accuracy and consistency for analysis.
    
                                          **Key Steps**
    Remove Duplicates: Identified duplicates using ROW_NUMBER() and removed them via staging tables.
    Standardise Data:
    Trimmed spaces and fixed inconsistencies in text (e.g., "CryptoCurrency" → "Crypto").
    Corrected date formats and updated country names (e.g., "United States." → "United States").
    Handle Null Values: Populated missing values by cross-referencing company names and replaced empty strings with NULL.
    Drop Unnecessary Data: Removed rows with critical null values and dropped redundant columns.
                                 **Techniques Used**
    Common Table Expressions (CTEs)
    Window Functions (ROW_NUMBER())
    Data Standardization (TRIM, STR_TO_DATE)
                                       **Purpose**
    To prepare clean, reliable data for analysis by eliminating inconsistencies and inaccuracies.

--- DATA CLEANING PROJECT IN MYSQL.sql
+++ DATA CLEANING PROJECT IN MYSQL.sql
@@ -1,4 +1,4 @@
--- DATA CLEANING PROJECT
+                  -- **DATA CLEANING PROJECT**
 
 
 -- STEPS 
@@ -11,7 +11,7 @@
 SELECT *
 FROM layoffs;
 
--- create a stage table off the raw data
+-- a.create a stage table off the raw data
 
 CREATE TABLE layoffs_staging
 LIKE layoffs;
@@ -23,14 +23,14 @@ INSERT layoffs_staging
 SELECT *
 FROM layoffs;
 
--- GIVE THEM AN ID NUMBER BY USING THE ROW NUMBER
+-- b. GIVE THEM AN ID NUMBER BY USING THE ROW NUMBER
 
 SELECT *,
 ROW_NUMBER() OVER (
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
 FROM layoffs_staging;
 
--- CREATE A CTE
+-- c. CREATE A CTE
 
 WITH duplicate_cte AS
 ( 
@@ -43,7 +43,7 @@ SELECT *
 FROM duplicate_cte
 WHERE row_num > 1;
 
--- to delete dupliate we have to create another table
+-- d. to delete dupliate we have to create another table because we cant update a cte
 
 CREATE TABLE `layoffs_staging2` (
   `company` text,
@@ -75,3 +75,155 @@ WHERE row_num > 1;
 DELETE
 FROM layoffs_staging2
 WHERE row_num > 1;
+
+
+-- 2. STANDARDIZE THE DATA i.e  
+SELECT *
+FROM layoffs_staging2;
+
+-- a. trim
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+
+-- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
+SELECT DISTINCT industry
+FROM world_layoffs.layoffs_staging2
+ORDER BY industry;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry IN ('Crypto Currency', 'CryptoCurrency');
+
+-- now that's taken care of:
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY industry;
+
+-- --------------------------------------------------
+-- we also need to look at 
+
+SELECT *
+FROM world_layoffs.layoffs_staging2;
+
+-- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
+SELECT DISTINCT country
+FROM world_layoffs.layoffs_staging2
+ORDER BY country;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country);
+
+-- now if we run this again it is fixed
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY country;
+
+
+-- Let's also fix the date columns:
+SELECT *
+FROM world_layoffs.layoffs_staging2;
+
+-- we can use str to date to update this field
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+-- now we can convert the data type properly
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+
+SELECT *
+FROM layoffs_staging2;
+
+-- 3. handleing null values
+-- if we look at industry it looks like we have some null and empty rows, let's take a look at these
+SELECT DISTINCT industry
+FROM world_layoffs.layoffs_staging2
+ORDER BY industry;
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+-- let's take a look at these
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Bally%';
+-- nothing wrong here
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE company LIKE 'airbnb%';
+
+-- it looks like airbnb is a travel, but this one just isn't populated.
+-- I'm sure it's the same for the others. What we can do is
+-- write a query that if there is another row with the same company name, it will update it to the non-null industry values
+-- makes it easy so if there were thousands we wouldn't have to manually check them all
+
+-- we should set the blanks to nulls since those are typically easier to work with
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+-- now if we check those are all null
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+-- now we need to populate those nulls if possible
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+-- and if we check it looks like Bally's was the only one without a populated row to populate this null values
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+-- ---------------------------------------------------
+
+
+-- 4. remove any columns and rows we need to
+
+SELECT *
+FROM .layoffs_staging2
+WHERE total_laid_off IS NULL;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+-- Delete Useless data we can't really use
+DELETE FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+SELECT * 
+FROM layoffs_staging2;
